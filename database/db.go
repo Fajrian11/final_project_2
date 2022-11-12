525f40947b19e97c622769f4c257ff27bcc44c88
unknown <nugraha.fajrian@gmail.com>
2022-11-12 15:52:42 +0700

    edit env 2

--- database/db.go
+++ database/db.go
@@ -10,8 +10,8 @@ import (
 )
 
 func DBinit(dbHost, dbPort, dbUsername, dbPassword, dbName string) *gorm.DB {
-	dsn := fmt.Sprintf("%s:%s@%s:%s/%s?charset=utf8&parseTime=True", dbUsername, dbPassword, dbHost, dbPort, dbName)
-	//root:iNdkC2NQNZ2bdQJhmwnE@containers-us-west-119.railway.app:7261/railway
+	dsn := fmt.Sprintf("%s:%s@%s:%s/%s", dbUsername, dbPassword, dbHost, dbPort, dbName)
+	//root:qF0Z1vu4M7yoJHdil1ch@containers-us-west-120.railway.app:7457/railway
 	fmt.Println(dsn)
 	db, err := gorm.Open("mysql", dsn)
 	if err != nil {
