76ac7750d0ab822c73eb2ab093d7a12b4bf52b6e
unknown <nugraha.fajrian@gmail.com>
2022-11-12 16:00:03 +0700

    edit env 3

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"FP2/router"
+	"log"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -10,5 +11,12 @@ import (
 func main() {
 	r := router.StartAPP()
 	port := os.Getenv("SERVICE_PORT")
-	r.Run(port)
+	if port == "" {
+		port = "9000" // Default port if not specified
+	}
+	err := r.Run()
+	if err != nil {
+		log.Println("[ERROR GRACEFUL]", err)
+		os.Exit(1)
+	}
 }
