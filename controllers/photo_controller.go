19396a135c61531a84909efea4566344e09577a5
unknown <nugraha.fajrian@gmail.com>
2022-11-12 17:57:31 +0700

    fixing bugs : create photo error

--- controllers/photo_controller.go
+++ controllers/photo_controller.go
@@ -15,22 +15,22 @@ func NewPhotoController(psa service.PhotoServiceApi) *PhotoController {
 	return &PhotoController{psa: psa}
 }
 
-func (uc *PhotoController) CreatePhotoControllers(c *gin.Context) {
-	res := uc.psa.CreatePhotoService(c)
+func (pc *PhotoController) CreatePhotoControllers(c *gin.Context) {
+	res := pc.psa.CreatePhotoService(c)
 	c.JSON(http.StatusOK, res)
 }
 
-func (uc *PhotoController) GetPhotoControllers(c *gin.Context) {
-	res := uc.psa.GetAllPhotoService(c)
+func (pc *PhotoController) GetPhotoControllers(c *gin.Context) {
+	res := pc.psa.GetAllPhotoService(c)
 	c.JSON(http.StatusOK, res)
 }
 
-func (uc *PhotoController) UpdatePhotoControllers(c *gin.Context) {
-	res := uc.psa.UpdatePhotoService(c)
+func (pc *PhotoController) UpdatePhotoControllers(c *gin.Context) {
+	res := pc.psa.UpdatePhotoService(c)
 	c.JSON(http.StatusOK, res)
 }
 
-func (uc *PhotoController) DeletePhotoControllers(c *gin.Context) {
-	res := uc.psa.DeletePhotoService(c)
+func (pc *PhotoController) DeletePhotoControllers(c *gin.Context) {
+	res := pc.psa.DeletePhotoService(c)
 	c.JSON(http.StatusOK, res)
 }
